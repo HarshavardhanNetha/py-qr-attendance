4864055c1435903ba46513fc1a00bad7746789cc
Harsha <harsha@stunxt.com>
2020-09-27 15:02:43 +0530

    Added finla step

--- take_attendance.py
+++ take_attendance.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 from playsound import playsound
+import openpyxl
+import datetime as dt
 
 record = cv2.VideoCapture(0)
 record.set(3,640)
@@ -14,28 +16,46 @@ while(1):
     for qr in decode(img):
         data=qr.data.decode('utf-8')
         #print(data)
-        if data not in present_data:
-            present_data.append(data)
-            #print(10*'\a')
-            playsound('beep.mp3')
 
         #box around
         pts = np.array([qr.polygon],np.int32)
         pts = pts.reshape((-1,1,2))
-        color=(255,0,255)
-        cv2.polylines(img,[pts],1,color,5)
-
+        
         #display on image
         pts2 = qr.rect
-        cv2.putText(img,data,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_PLAIN, 1,color,2)
+        
+        if data not in present_data:
+            present_data.append(data)
+            #print(10*'\a')
+            playsound('beep.mp3')
+            color=(0,255,0)
+            cv2.polylines(img,[pts],1,color,5)
+            #cv2.waitKey(3)
+        else:
+            color=(0,0,255)    
+            cv2.polylines(img,[pts],1,color,5)
+            cv2.putText(img,"Thank You! {}".format(data),(pts2[0],pts2[1]),cv2.FONT_HERSHEY_PLAIN, 1,color,2)
 
     cv2.imshow('Result',img)
     key = cv2.waitKey(1)
 
     #to release camera frame
-    if(key==27):
+    if(key==27): #esc key value is 27
         cv2.destroyWindow("Result")
         record.release()
         break
     
 print(present_data)
+
+temp = openpyxl.load_workbook("Attendance_Sheet.xlsx")
+sheet = temp.active
+column = sheet.max_column
+sheet.cell(1,column+1).value = dt.date.today()
+for i in range(2,sheet.max_row+1):
+    x=sheet.cell(i,2)
+    if(x.value in present_data):
+       sheet.cell(i,column+1).value="P"
+    else:
+        sheet.cell(i,column+1).value="Ab"
+
+temp.save("Attendance_Sheet.xlsx")
