097608160b951dfba54c34420d6bd9fd2ac94c60
Harsha <harsha@stunxt.com>
2020-09-27 16:56:51 +0530

    Online Spreadsheet

--- take_attendance.py
+++ take_attendance.py
@@ -4,6 +4,9 @@ from pyzbar.pyzbar import decode
 from playsound import playsound
 import openpyxl
 import datetime as dt
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 
 record = cv2.VideoCapture(0)
 record.set(3,640)
@@ -28,11 +31,11 @@ while(1):
             present_data.append(data)
             #print(10*'\a')
             playsound('beep.mp3')
-            color=(0,255,0)
+            color=(0,0,255)
             cv2.polylines(img,[pts],1,color,5)
             #cv2.waitKey(3)
         else:
-            color=(0,0,255)    
+            color=(0,255,0)    
             cv2.polylines(img,[pts],1,color,5)
             cv2.putText(img,"Thank You! {}".format(data),(pts2[0],pts2[1]),cv2.FONT_HERSHEY_PLAIN, 1,color,2)
 
@@ -47,7 +50,8 @@ while(1):
     
 print(present_data)
 
-temp = openpyxl.load_workbook("Attendance_Sheet.xlsx")
+#offline
+'''temp = openpyxl.load_workbook("Attendance_Sheet.xlsx")
 sheet = temp.active
 column = sheet.max_column
 sheet.cell(1,column+1).value = dt.date.today()
@@ -58,4 +62,33 @@ for i in range(2,sheet.max_row+1):
     else:
         sheet.cell(i,column+1).value="Ab"
 
-temp.save("Attendance_Sheet.xlsx")
+temp.save("Attendance_Sheet.xlsx") '''
+
+#online Google Sheets
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('attendance.json', scope)
+client = gspread.authorize(creds)
+wb = client.open('Attendance_Sheet')
+sheet = wb.get_worksheet(0)
+
+#column = sheet.cell(100,2).value
+max_row = len(sheet.get_all_values())
+max_col = len(sheet.get_all_values()[0])
+
+#print(column,type(column))
+#max_col=int(column)
+#print(max_col,type(max_col))
+
+to_day = str(dt.date.today())
+#print(to_day,type(to_day))
+
+sheet.update_cell(1,max_col+1,to_day)
+
+#sheet.cell(1,column+1).value = dt.date.today()
+for i in range(2,max_row+1):
+    x=sheet.cell(i,2)
+    if(x.value in present_data):
+        sheet.update_cell(i,max_col+1,"P")
+    else:
+        sheet.update_cell(i,max_col+1,"Ab")
+#sheet.update_cell(100,2,max_col+1)
